b5b14337b3beeec085facd9cf017661dfb47a7bd
jaeheung09 <48986338+jaeheung09@users.noreply.github.com>
2019-04-27 20:54:52 +0900

    Update D3CombatLogPlugin.cs

--- D3CombatLogPlugin.cs
+++ D3CombatLogPlugin.cs
@@ -45,7 +45,7 @@ namespace Turbo.Plugins.James
         	private const int LatencyBad = 80;		// if cur latency is 80 ms
         	private const int FPSWarning = 40;		// if current FPS is less than 40
         	private const int FPSBad = 20;			// if cur FPS is less than 20
-        	private const int RMInterval = 1000; 		// Resources monitoring interval 1ÃÊ
+        	private const int RMInterval = 1000; 		// Resources monitoring interval 1ì´ˆ
         	//*********************************************************************************************************
         	private string culture;
         	private string eliteNames;
@@ -62,7 +62,6 @@ namespace Turbo.Plugins.James
 		private bool IsGRift;
 		private bool IsBoss;
 		private uint savedSno;
-		private string BossTextUiName = "Root.NormalLayer.game_notify_dialog_backgroundScreen.game_text_line0";
 		private string chatEditLine = "Root.NormalLayer.chatentry_dialog_backgroundScreen.chatentry_content.chat_editline";
 		// private enum display { ok, warning, bad }
 
@@ -127,12 +126,12 @@ namespace Turbo.Plugins.James
                if (culture == "ko")
                {
 				BuffsToWatch = new List<Buff>();
-				BuffsToWatch.Add(new Buff(262935, 0, "´É·ÂP"));
-				BuffsToWatch.Add(new Buff(266258, 0, "Àç°¨P"));
-				BuffsToWatch.Add(new Buff(266254, 0, "º¸È£P"));
-				BuffsToWatch.Add(new Buff(263029, 0, "µµ°üP"));
-				BuffsToWatch.Add(new Buff(403404, 0, "µµ°üP"));
-				BuffsToWatch.Add(new Buff(266271, 0, "¼ÓµµP"));
+				BuffsToWatch.Add(new Buff(262935, 0, "ëŠ¥ë ¥P"));
+				BuffsToWatch.Add(new Buff(266258, 0, "ì¬ê°P"));
+				BuffsToWatch.Add(new Buff(266254, 0, "ë³´í˜¸P"));
+				BuffsToWatch.Add(new Buff(263029, 0, "ë„ê´€P"));
+				BuffsToWatch.Add(new Buff(403404, 0, "ë„ê´€P"));
+				BuffsToWatch.Add(new Buff(266271, 0, "ì†ë„P"));
 			} else
 			{
 				BuffsToWatch = new List<Buff>();
@@ -219,14 +218,14 @@ namespace Turbo.Plugins.James
 
         				if (Hud.Sound.LastSpeak.TimerTest(5000))
         					if (culture == "ko")
-        						Hud.Sound.Speak("ÀüÅõ ·Î±× ¿äÃ»ÀÌ Á¢¼öµÇ¾ú½À´Ï´Ù.");
+        						Hud.Sound.Speak("ì „íˆ¬ ë¡œê·¸ ìš”ì²­ì´ ì ‘ìˆ˜ë˜ì—ˆìŠµë‹ˆë‹¤.");
         					else
         						Hud.Sound.Speak("your combat log request is received.");
         			} else
         			{
         				if (Hud.Sound.LastSpeak.TimerTest(5000))
         					if (culture == "ko")
-        						Hud.Sound.Speak("·Î±× ¿äÃ»ÀÌ ÀÌ¹Ì Á¢¼öµÈ »óÅÂÀÔ´Ï´Ù.");
+        						Hud.Sound.Speak("ë¡œê·¸ ìš”ì²­ì´ ì´ë¯¸ ì ‘ìˆ˜ëœ ìƒíƒœì…ë‹ˆë‹¤.");
         					else
         						Hud.Sound.Speak("Your log request is already received.");
         			}
@@ -237,14 +236,14 @@ namespace Turbo.Plugins.James
         		     		Logging = false;
         		     		if (Hud.Sound.LastSpeak.TimerTest(5000))
         		     			if (culture == "ko")
-        						Hud.Sound.Speak("ÀüÅõ ·Î±× ¿äÃ»ÀÌ Ãë¼ÒµÇ¾ú½À´Ï´Ù.");
+        						Hud.Sound.Speak("ì „íˆ¬ ë¡œê·¸ ìš”ì²­ì´ ì·¨ì†Œë˜ì—ˆìŠµë‹ˆë‹¤.");
         					else
         						Hud.Sound.Speak("your log request is canceled.");
         			} else
         			{
         				if (Hud.Sound.LastSpeak.TimerTest(5000))
         					if (culture == "ko")
-        						Hud.Sound.Speak("·Î±× ¿äÃ»ÀÌ Á¢¼öµÇÁö ¾ÊÀº »óÅÂÀÔ´Ï´Ù.");
+        						Hud.Sound.Speak("ë¡œê·¸ ìš”ì²­ì´ ì ‘ìˆ˜ë˜ì§€ ì•Šì€ ìƒíƒœì…ë‹ˆë‹¤.");
         					else
         						Hud.Sound.Speak("No log request exists..");
         			}
@@ -326,7 +325,7 @@ namespace Turbo.Plugins.James
 			          	//}
 			          	Hud.TextLog.Log(LogFile, titleContent, aTimeStamp, true);
 			          	if (culture == "ko")
-			          		Hud.Sound.Speak("·Î±× ±â·ÏÀÌ ¿Ï·áµÇ¾ú½À´Ï´Ù!");
+			          		Hud.Sound.Speak("ë¡œê·¸ ê¸°ë¡ì´ ì™„ë£Œë˜ì—ˆìŠµë‹ˆë‹¤!");
 			          	else
 			          		Hud.Sound.Speak("Combat Log is completed!");
 			          		
@@ -360,7 +359,7 @@ namespace Turbo.Plugins.James
 				int riftlevel = 0;
 				if (Hud.Game.SpecialArea == SpecialArea.Rift)
 				{
-					sArea = (culture == "ko") ? "ÀÏ±Õ" : "Rift";
+					sArea = (culture == "ko") ? "ì¼ê· " : "Rift";
 					var tmp = Convert.ToString(Hud.Game.GameDifficulty);
 					Match matchL = Regex.Match(tmp, @"(\d){1,}");
 					if (matchL.Success)
@@ -369,7 +368,7 @@ namespace Turbo.Plugins.James
 						riftlevel = 0;
 				} else if (Hud.Game.SpecialArea == SpecialArea.GreaterRift)
 				{
-					sArea = (culture == "ko") ? "´ë±Õ" : "GRift";
+					sArea = (culture == "ko") ? "ëŒ€ê· " : "GRift";
 					riftlevel = (int)Hud.Game.Me.InGreaterRiftRank;		// Grift tier
 				}
 				try {
@@ -397,7 +396,7 @@ namespace Turbo.Plugins.James
 				if (match.Success)
 				{
 					if (culture == "ko")
-						area = "ÁöÇÏ " + match.Value + "Ãş";
+						area = "ì§€í•˜ " + match.Value + "ì¸µ";
 					else
 						area = "Run Level " + match.Value;
 				}
@@ -422,7 +421,6 @@ namespace Turbo.Plugins.James
 						titleContent += ", " + buff.Name;
 	            	}
 
-				//if (Hud.Render.GetUiElement(BossTextUiName).Visible)	// for GR
 				if (IsBoss)
 		          	{
 		          		if (BossFirst)
